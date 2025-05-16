f4e69bcbc55011b3b1c3fab1ee0c2193cd7f3530
yath-rth <thecreator106@gmail.com>
2025-05-16 10:10:17 +0530

    Merging Controller

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -3,5 +3,34 @@ using UnityEngine.InputSystem;
 
 public class Player_Movement : MonoBehaviour
 {
+    [SerializeField] private float speed = 5f;
+
     InputSystem_Actions inputAction;
+    CharacterController controller;
+    Vector2 moveInput;
+
+    void Awake()
+    {
+        if (inputAction == null) inputAction = new InputSystem_Actions();
+        inputAction.Player.Enable();
+
+        controller = GetComponent<CharacterController>();
+    }
+
+    void OnEnable()
+    {
+        inputAction.Enable();
+    }
+
+    void OnDisable()
+    {
+        inputAction.Disable();
+    }
+
+    void Update()
+    {
+        moveInput = inputAction.Player.Move.ReadValue<Vector2>();
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+        controller.Move(move * Time.deltaTime * speed);
+    }
 }
